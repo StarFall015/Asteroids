87a59791b5a7f75f51d901c2f9579d8b088449cf
Kieran Bradbury <kieranbradbury015@hotmail.com>
2025-05-31 15:52:19 +0100

    create classes for asteroids and player (player not moving properly)

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import pygame
 from constants import *
-
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(255,255,255)
+        screen.fill((0,0,0))
+
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
